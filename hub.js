5fd41427f95bf1ff800a2c61677ca8a11c5259c6
aayush301-coder <aayushgarg301@gmail.com>
2026-03-22 12:02:57 +0530

    Added resource rendering

--- hub.js
+++ hub.js
@@ -4,6 +4,7 @@ const form = document.getElementById("resources-form");
 const titleInput = document.getElementById("title");
 const linkInput = document.getElementById("link");
 const categoryInput = document.getElementById("category");
+const container = document.getElementById("resources-container");
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -17,5 +18,25 @@ form.addEventListener("submit", function (e) {
     };
 
     resources.push(resource);
-    console.log(resources);
+
+    titleInput.value = "";
+    linkInput.value = "";
+    categoryInput.value = "";
+
+    renderResources();
 });
+
+function renderResources() {
+    container.innerHTML = "";
+    resources.forEach(function(item) {
+        const div = document.createElement("div");
+        div.classList.add("resource-card");
+        div.innerHTML = `
+            <h3>${item.title}</h3>
+            <a href="${item.link}" target="_blank">Open</a>
+            <p>${item.category}</p>
+        `;
+
+        container.appendChild(div);
+    });
+}
